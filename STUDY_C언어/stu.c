5ba877f813059d7e65728830f4bfa934eca487bc
Chahaneum <welfare0320@naver.com>
2020-07-26 22:37:28 +0900

    call-by-value와reference 포인터 함수 공부.1n

--- STUDY_C언어/stu.c
+++ STUDY_C언어/stu.c
@@ -1,21 +1,22 @@
 ﻿#include <stdio.h>
+int Sq(int num)//call-by-value
+{
+		return num * num;
+	
+}
+int Sqb(int* ptr)//call-by-reference
+{
+	int num = *ptr;
+
+	*ptr = num * num;
+}
 
 int main(void)
 {
-	int arr[5] = { 1, 2, 3, 4, 5 };
-	int* ptr = arr;
+	int num = 12;
 
-	int i;
-	for (i = 0; i < 5; i++)
-	{
-		*ptr += 2;
-		ptr++;
-	}
-	for (i = 0; i < 5; i++)
-	{
-		printf("%d\n", arr[i]);
-	}
-
+	printf("num의 제곱 : %d\n", Sq(num));
+	Sqb(&num);//Sqb가 num을 가리키게 한다(포인터라서..)
+	printf("num의 제곱 : %d\n", num);
 	return 0;
-	//이해 ok;
 }
