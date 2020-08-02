2abd27b13250b34f2dd256880ce1b4db8fc72c3b
Chahaneum <welfare0320@naver.com>
2020-08-03 00:41:13 +0900

    이중 포인터 공부

--- STUDY_C언어/stu.c
+++ STUDY_C언어/stu.c
@@ -1,25 +1,46 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS//scanf보안 오류.
+#include <stdio.h>
+
+void MaxAndMin(int *arr,int size, int **maxP, int **minP)
+{
+	int* max;
+	int* min;
+	int i;
+
+	max = min = &arr[0];//arr[0] = 1이다(입력된 경우) 즉 max = 1, min = 1, arr[0]=1이다.
+	for (i = 0; i < size; i++)
+	{
+	
+	//노트 c언어 구역 참고!
+		if (*max < arr[i])//12345입력이 진행됬다면 1 < 1 / 1 < 2 / 1 < 3 / 1 < 4 / 1<5/이니 이것을 실행하고 마지막 5를 반환
+		{
+			max = &arr[i];//max = arr[4]=5
+		}
+		if (*min > arr[i])
+		{
+			min = &arr[i];
+		}
+	}
+
+	*maxP = max;
+	*minP = min;
+}
 
 int main(void)
 {
-	int N[3][9];
-	int i, j;
+	int* maxPtr;
+	int* minPtr;
+	int arr[5];
+	int i;
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 5; i++)
 	{
-		for (j = 0; j < 9; j++)
-		{
-			N[i][j] = (i + 2) * (j + 1);
-		}
+		printf("정수를 입력 >>");
+		scanf("%d", &arr[i]);
 	}
+	printf("arr = %d", sizeof(arr));//20임을 확인. 5x4 = 20;
+	MaxAndMin(arr, sizeof(arr) / sizeof(int), &maxPtr, &minPtr);
 
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 9; j++)
-		{
-			printf("%10d", N[i][j]);//%xd = 보여지는 값의 떨어진 값 %7d는 왼쪽부터 스페이즈바 7번한 것의 있다.
-			printf("\n");
-		}
-	}
+	printf("최대 : %d, 최소 : %d\n", *maxPtr, *minPtr);
 	return 0;
 }
