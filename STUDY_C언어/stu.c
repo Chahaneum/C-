27a82b9ee07f49a8525f1e407b83cd304c877f6d
Chahaneum <welfare0320@outlook.kr>
2020-08-05 22:03:25 +0900

    알고리즘 내림차순 정리 공부 -다시 공부-

--- STUDY_C언어/stu.c
+++ STUDY_C언어/stu.c
@@ -1,46 +1,42 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS//scanf보안 오류.
 #include <stdio.h>
 
-void MaxAndMin(int *arr,int size, int **maxP, int **minP)
+int DesSort(int arr[], int len)
 {
-	int* max;
-	int* min;
-	int i;
+	int i, j;
+	int te;
 
-	max = min = &arr[0];//arr[0] = 1이다(입력된 경우) 즉 max = 1, min = 1, arr[0]=1이다.
-	for (i = 0; i < size; i++)
+
+	for (i = 0; i < len - 1; i++)
 	{
-	
-	//노트 c언어 구역 참고!
-		if (*max < arr[i])//12345입력이 진행됬다면 1 < 1 / 1 < 2 / 1 < 3 / 1 < 4 / 1<5/이니 이것을 실행하고 마지막 5를 반환
+		for (j = 0; j < (len - i) - 1; j++)
 		{
-			max = &arr[i];//max = arr[4]=5
-		}
-		if (*min > arr[i])
-		{
-			min = &arr[i];
+			if (arr[j] < arr[j + 1])
+			{
+				te = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = te;
+			}
 		}
 	}
-
-	*maxP = max;
-	*minP = min;
 }
-
 int main(void)
 {
-	int* maxPtr;
-	int* minPtr;
-	int arr[5];
+	int arr[7];
 	int i;
 
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 7; i++)
 	{
-		printf("정수를 입력 >>");
+		printf("\n정수를 입력)>>");
 		scanf("%d", &arr[i]);
 	}
-	printf("arr = %d", sizeof(arr));//20임을 확인. 5x4 = 20;
-	MaxAndMin(arr, sizeof(arr) / sizeof(int), &maxPtr, &minPtr);
 
-	printf("최대 : %d, 최소 : %d\n", *maxPtr, *minPtr);
+	DesSort(arr, sizeof(arr) / sizeof(int));
+
+	for (i = 0; i < 7; i++)
+	{
+		printf("내림차순으로 정렬된것 : %d \n", arr[i]);
+
+	}
 	return 0;
 }
