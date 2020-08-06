8741afc96ab96e76dbcb00d9f129d9e426d5819d
Chahaneum <welfare0320@outlook.kr>
2020-08-07 01:02:18 +0900

    독학 이해 더블 포인터변수

--- STUDY_C언어/stu.c
+++ STUDY_C언어/stu.c
@@ -1,1 +1,12 @@
-﻿
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int h[2][2] = { {1,2}, {2,3 } };
+	int(*ptr)[5] = h;
+	printf("%d", h[1][1]);
+	return 0;
+	//my self()이해 OK!
+
+}
