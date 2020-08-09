eef06dc62b24e227b57066e2582f8bf30f2af679
Chahaneum <welfare0320@outlook.kr>
2020-08-09 22:28:38 +0900

    strcmp & strncmp 공부

--- STUDY_C언어/활용.c
+++ STUDY_C언어/활용.c
@@ -1,20 +1,35 @@
-﻿
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+//strcmp 공부
 int main(void)
 {
-	int ch;
+	char str[20];
+	char stra[20];
+	printf("문자열 입력 : ");
+	scanf("%s", str);
+	printf("문자열 입력 : ");
+	scanf("%s", stra);
 
-	while (1)
+	if (!strcmp(str, stra))
 	{
-		ch = getchar();
-
-		if (ch == EOF)
-		{
-			break;
-		}
-		putchar(ch);
-
+		puts("두문자열은 완벽히 동일한 문장!");
 	}
-	return 0;//
+	else
+	{
+		puts("동일하지 않음");
+
+		if (!strncmp(str, stra, 2))
+		{
+			puts("그치만 2글자는 맞음");
+		}
+	}
+	
+	
+	
+	
+	
+	return 0;
 }
