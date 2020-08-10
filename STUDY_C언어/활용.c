fa4d851dbd681d545ceb83e92ff85123164db504
Chahaneum <welfare0320@outlook.kr>
2020-08-10 23:34:37 +0900

    구조체 배열 공부

--- STUDY_C언어/활용.c
+++ STUDY_C언어/활용.c
@@ -1,35 +1,40 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-//strcmp 공부
 int main(void)
 {
-	char str[20];
-	char stra[20];
-	printf("문자열 입력 : ");
-	scanf("%s", str);
-	printf("문자열 입력 : ");
-	scanf("%s", stra);
-
-	if (!strcmp(str, stra))
+	struct employee
 	{
-		puts("두문자열은 완벽히 동일한 문장!");
-	}
-	else
-	{
-		puts("동일하지 않음");
+		char a[10];//name
+		char b[20];//주번
+		int c;//급여
+	};
 
-		if (!strncmp(str, stra, 2))
-		{
-			puts("그치만 2글자는 맞음");
-		}
+	struct employee u[3];//구조체이름 쓰고 주고체 변수 선언(이름) 해야된다!
+	int i;
+
+	for (i = 0; i < 3; i++)
+	{
+		printf("이름 입력 >");
+		scanf("%s", u[i].a);
+
+		printf("\n");
+
+		printf("주민등록번호 입력 >");
+		scanf("%s", u[i].b);
+
+		printf("\n");
+
+		printf("급여를 입력 (원) >");
+		scanf("%d", &u[i].c);
+
+		printf("[%d번 남았습니다.]\n",i+1);
 	}
-	
-	
-	
-	
-	
+
+	for (i = 0; i < 3; i++)
+	{
+		printf("%d분의 정보 : 이름 [%s] 주민등록번호 [%s] 급여 [%d] \n", i + 1, u[i].a, u[i].b, u[i].c);
+	}
 	return 0;
+	//test완료
 }
